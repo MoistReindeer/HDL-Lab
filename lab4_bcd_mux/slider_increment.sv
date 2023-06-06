9a5e9b646e216f5d9c6ffd20145b7f7a5218e472
Tobias <tobiasdeutsch@pm.me>
2023-06-06 10:25:03 +0200

    Implemented number increment based on slider

--- lab4_bcd_mux/slider_increment.sv
+++ lab4_bcd_mux/slider_increment.sv
@@ -1,10 +1,12 @@
 module slider_increment (
     input logic clk,
+    input logic rst_ext,
     input logic slider_1,
     input logic slider_2,
     input logic slider_3,
     input logic slider_4,
-    input logic rst_ext
+    output logic [13:0] number_1,
+    output logic [13:0] number_2
 );
 
     logic rst_int = 0;
@@ -33,6 +35,18 @@ module slider_increment (
             cnt_3 <= cnt_3 + 1;
         else if (slider_4)
             cnt_4 <= cnt_4 + 1;
+    end
+
+    // Increment based on active slider, default max: 32500000
+    always_ff @( posedge clk ) begin
+        if(cnt_1 == 32500000)
+            number_1 <= number_1 + 1;
+        else if(cnt_2 == 32500000)
+            number_1 <= number_1 + 10;
+        else if(cnt_3 == 32500000)
+            number_1 <= number_1 + 10;
+        else if(cnt_4 == 32500000)
+            number_1 <= number_1 + 10;
     end
 
 endmodule
