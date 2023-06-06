9af7dbfdf790ddbe7b09f47bf088c373e4fa7ea6
Tobias <tobiasdeutsch@pm.me>
2023-06-06 10:25:03 +0200

    Fixed linting issues

--- lab4_bcd_mux/seg_dec.sv
+++ lab4_bcd_mux/seg_dec.sv
@@ -6,17 +6,17 @@ module seg_dec (
     always_comb
     begin
         case (disp_num)
-            0   :   bcn = 8'b1111110;
-            1   :   bcn = 8'b0110000;
-            2   :   bcn = 8'b1101101;
-            3   :   bcn = 8'b1111001;
-            4   :   bcn = 8'b0110011;
-            5   :   bcn = 8'b1011011;
-            6   :   bcn = 8'b1011110;
-            7   :   bcn = 8'b1110000;
-            8   :   bcn = 8'b1111111;
-            9   :   bcn = 8'b1110011;
-            default : bcn = 8'bxxxxxxx;
+            0   :   bcn = 7'b1111110;
+            1   :   bcn = 7'b0110000;
+            2   :   bcn = 7'b1101101;
+            3   :   bcn = 7'b1111001;
+            4   :   bcn = 7'b0110011;
+            5   :   bcn = 7'b1011011;
+            6   :   bcn = 7'b1011110;
+            7   :   bcn = 7'b1110000;
+            8   :   bcn = 7'b1111111;
+            9   :   bcn = 7'b1110011;
+            default : bcn = 7'bxxxxxxx;
         endcase
     end
 endmodule
--- lab4_bcd_mux/seg_dec_tb.sv
+++ lab4_bcd_mux/seg_dec_tb.sv
@@ -1,3 +1,4 @@
+/* verilator lint_off UNUSEDSIGNAL */
 module seg_dec_tb ( );
     timeprecision 1ns;
 
@@ -5,20 +6,20 @@ module seg_dec_tb ( );
     logic [3:0] number;
     logic [6:0] led_select;
 
-    always #5ns clk = ~clk;
+    always #5ns clk <= ~clk;
 
     seg_dec decoder(
                     .disp_num(number),
                     .bcn(led_select));
 
     initial begin
-        $dumpfile("./build/data.vcd");
+        $dumpfile("./build/seg_dec.vcd");
         $dumpvars(0, decoder);
         #1ns;
 
         for (int i = 0; i < 4; i++)
         begin
-            for (int j = 0; j < 10; j++)
+            for (logic [3:0] j = 0; j < 10; j++)
             begin
                 assign number = j;
                 #2ns;
