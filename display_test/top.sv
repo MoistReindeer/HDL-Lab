a6672ff74beab997cad812e7fd3c38bcf6f5eb6c
VeryMoistReindeer <tobiasdeutsch@pm.me>
2023-06-08 09:56:51 +0200

    Top-module now implements the mux and decoder

--- display_test/top.sv
+++ display_test/top.sv
@@ -1,22 +1,26 @@
-/* verilator lint_off WIDTHTRUNC */
 module top #(
     parameter REFRESH_OVERFLOW = 2**19-1
-)(
+) (
     input logic clk,
     input logic reset,
-    input logic [13:0] sld,
+    input logic [3:0] split_nr[4],
     output logic [3:0] digit_select,
     output logic [6:0] led_select
 );
+    
+    logic [3:0] digit_display;
 
-    display #(
+    bcd_mux #(
         .REFRESH_OVERFLOW(REFRESH_OVERFLOW)
-    ) display (
+    ) MUX (
         .clk(clk),
         .reset(reset),
-        .to_display_nr(sld),
-        .digit_select(digit_select),
-        .led_select(led_select)
-    );
+        .number(split_nr),
+        .output_number(digit_display),
+        .digit_select(digit_select));
 
+    seg_dec seg_dec_mod (
+        .disp_num(digit_display),
+        .bcn(led_select));
+    
 endmodule
