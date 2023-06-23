1268429e7d14076d1ca57885c0a5574286970d1e
Tobias <tobiasdeutsch@pm.me>
2023-06-23 09:17:32 +0200

    Fixed slider and button issue

--- calculator/testbench/calculator_input_tb.sv
+++ calculator/testbench/calculator_input_tb.sv
@@ -3,7 +3,7 @@ module calculator_input_tb ();
     logic clk = 0;
     logic rst = 1;
 
-    always #5ns clk <= ~clk;
+    always #1ns clk <= ~clk;
 
     logic btn_clr_async = 0;
     logic btn_ent_async = 0;
@@ -21,12 +21,8 @@ module calculator_input_tb ();
         .reset(rst),
         .button_clr_undeb(btn_clr_async),
         .button_ent_undeb(btn_ent_async),
-        .button_add_undeb(btn_add_async),
-        .button_sub_undeb(btn_sub_async),
         .button_clr(btn_clr),
         .button_ent(btn_ent),
-        .button_add(btn_add),
-        .button_sub(btn_sub),
         .slider_1_undeb(sld_async[0]),
         .slider_2_undeb(sld_async[1]),
         .slider_3_undeb(sld_async[2]),
@@ -40,9 +36,13 @@ module calculator_input_tb ();
         $dumpfile("./build/calculator_input.fst");
         $dumpvars(0, DUT);
 
+        sld_async[0] = 0;
+        sld_async[1] = 0;
+        sld_async[2] = 0;
+        sld_async[3] = 0;
         #10ns;
         rst = 0;
-        #7ns;
+        #27ns;
         btn_clr_async = 1;
         btn_ent_async = 1;
         btn_add_async = 1;
